f7c62b19850f9b04636305d2d933cf61bcabb99e
b-gilley <141060801+b-gilley@users.noreply.github.com>
2023-08-17 12:10:37 +0100

    Completed Part 1 Tasks

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,11 +1,11 @@
 module.exports = {
-	title: "Eleventy Base Blog v8",
+	title: "Ben's Eleventy Blog",
 	url: "https://example.com/",
 	language: "en",
 	description: "I am writing about my experiences as a naval navel-gazer.",
 	author: {
-		name: "Your Name Here",
-		email: "youremailaddress@example.com",
+		name: "Benjamin Gilley",
+		email: "benji.gilley01@gmail.com",
 		url: "https://example.com/about-me/"
 	}
 }
---
+++ content/blog/sixthpost.md
@@ -0,0 +1,8 @@
+---
+title: This is the sixth post.
+description: This is a post to test creating a blogpost.
+date: 2023-07-01
+tags:
+  - another tag
+---
+lorem ipsum
